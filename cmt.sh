28ff19f5a5c46c5bedf186beef870cc630fadd2d
matdexir <matthieudesir21@gmail.com>
2023-04-23 21:41:08 +0800

    feat(cmt.sh):added support for breaking changes

--- cmt.sh
+++ cmt.sh
@@ -2,11 +2,12 @@
 
 TYPE=$(gum choose --cursor="◉ " "fix" "feat" "docs" "style" "ci" "refactor" "test" "chore" "revert")
 SCOPE=$(gum input --placeholder "scope")
+gum confirm "Breaking Changes?" && BREAKING="!"
 
 
 test -n  "$SCOPE" && SCOPE="($SCOPE)"
 
-SUMMARY=$(gum input --width 80 --value "$TYPE$SCOPE:" --placeholder "Quick summary of this change")
+SUMMARY=$(gum input --width 80 --value "$TYPE$BREAKING$SCOPE:" --placeholder "Quick summary of this change")
 DESCRIPTION=$(gum write --placeholder "Detailed description of this change (CTRL+D to finish)")
 
 gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION"
