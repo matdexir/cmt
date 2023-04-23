0711459c68a61b7b36d8e9a902c07eb3a946961e
matdexir <matthieudesir21@gmail.com>
2023-04-23 21:09:17 +0800

    fix(cmt.sh): expanded the width of the commit message input section

--- cmt.sh
+++ cmt.sh
@@ -6,7 +6,7 @@ SCOPE=$(gum input --placeholder "scope")
 
 test -n  "$SCOPE" && SCOPE="($SCOPE)"
 
-SUMMARY=$(gum input --value "$TYPE$SCOPE:" --placeholder "Quick summary of this change")
+SUMMARY=$(gum input --width 80 --value "$TYPE$SCOPE:" --placeholder "Quick summary of this change")
 DESCRIPTION=$(gum write --placeholder "Detailed description of this change (CTRL+D to finish)")
 
 gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION"
