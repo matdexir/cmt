922a5a79732df9465b0622aa95949fbfb94eab59
matdexir <matthieudesir21@gmail.com>
2023-04-23 21:31:44 +0800

    feat(cmt.sh):added ci as an option

--- cmt.sh
+++ cmt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TYPE=$(gum choose --cursor="◉ " "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
+TYPE=$(gum choose --cursor="◉ " "fix" "feat" "docs" "style" "ci" "refactor" "test" "chore" "revert")
 SCOPE=$(gum input --placeholder "scope")
 
 
